74fa1a4d34a6040ecf1359ef615b4639a72e8a7c
Tymofieiev <vovanium.timofeev@gmail.com>
2021-03-07 21:13:26 +0200

    build in directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,14 @@
-import {
-  Component,
-  EventEmitter
-} from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExampleDef } from './example.model';
 
-import {Product} from './product.model';
-
-/**
- * @InventoryApp: the top-level component for our application
- */
 @Component({
-  selector: 'inventory-app-root',
+  selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  products: Product[];
-
-  constructor() {
-    this.products = [
-      new Product(
-        'MYSHOES',
-        'Black Running Shoes',
-        '/assets/images/products/black-shoes.jpg',
-        ['Men', 'Shoes', 'Running Shoes'],
-        109.99),
-      new Product(
-        'NEATOJACKET',
-        'Blue Jacket',
-        '/assets/images/products/blue-jacket.jpg',
-        ['Women', 'Apparel', 'Jackets & Vests'],
-        238.99),
-      new Product(
-        'NICEHAT',
-        'A Nice Black Hat',
-        '/assets/images/products/black-hat.jpg',
-        ['Men', 'Accessories', 'Hats'],
-        29.99)
-    ];
-  }
-
-  productWasSelected(product: Product): void {
-    console.log('Product clicked: ', product);
+  constructor(
+    private router: Router,
+    @Inject('ExampleDefs') public examples: ExampleDef[]) {
   }
 }
