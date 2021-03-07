5a39cc0414a0998ec323b62b34b3299325804efd
Tymofieiev <vovanium.timofeev@gmail.com>
2021-03-07 21:30:55 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,58 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule, Routes, Router } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes, Router } from '@angular/router';
 import {
   APP_BASE_HREF,
   LocationStrategy,
   HashLocationStrategy
-} from "@angular/common";
+} from '@angular/common';
 
-import { AppComponent } from "./app.component";
-import { IntroComponent } from "./intro/intro.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
-import { SidebarItemComponent } from "./sidebar/sidebar-item.component";
-import { NgClassExampleComponent } from "./ng-class-example/ng-class-example.component";
-import { NgForExampleComponent } from "./ng-for-example/ng-for-example.component";
-import { NgNonBindableExampleComponent } from "./ng-non-bindable-example/ng-non-bindable-example.component";
-import { NgStyleExampleComponent } from "./ng-style-example/ng-style-example.component";
-import { NgSwitchExampleComponent } from "./ng-switch-example/ng-switch-example.component";
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarItemComponent } from './sidebar/sidebar-item.component';
+import { NgClassExampleComponent } from './ng-class-example/ng-class-example.component';
+import { NgForExampleComponent } from './ng-for-example/ng-for-example.component';
+import { NgNonBindableExampleComponent } from './ng-non-bindable-example/ng-non-bindable-example.component';
+import { NgStyleExampleComponent } from './ng-style-example/ng-style-example.component';
+import { NgSwitchExampleComponent } from './ng-switch-example/ng-switch-example.component';
 
-import { ExampleDef } from "./example.model";
+import { ExampleDef } from './example.model';
 
 /*
  * Here's the master list of our examples for this chapter.
  */
 export const examples: ExampleDef[] = [
-  { label: "Intro", name: "Root", path: "", component: IntroComponent },
+  { label: 'Intro', name: 'Root', path: '', component: IntroComponent },
   {
-    label: "NgFor",
-    name: "NgFor",
-    path: "ng_for",
+    label: 'NgFor',
+    name: 'NgFor',
+    path: 'ng_for',
     component: NgForExampleComponent
   },
   {
-    label: "NgSwitch",
-    name: "NgSwitch",
-    path: "ng_switch",
+    label: 'NgSwitch',
+    name: 'NgSwitch',
+    path: 'ng_switch',
     component: NgSwitchExampleComponent
   },
   {
-    label: "NgStyle",
-    name: "NgStyle",
-    path: "ng_style",
+    label: 'NgStyle',
+    name: 'NgStyle',
+    path: 'ng_style',
     component: NgStyleExampleComponent
   },
   {
-    label: "NgClass",
-    name: "NgClass",
-    path: "ng_class",
+    label: 'NgClass',
+    name: 'NgClass',
+    path: 'ng_class',
     component: NgClassExampleComponent
   },
   {
-    label: "NgNonBindable",
-    name: "NgNonBindable",
-    path: "ng_non_bindable",
+    label: 'NgNonBindable',
+    name: 'NgNonBindable',
+    path: 'ng_non_bindable',
     component: NgNonBindableExampleComponent
   }
 ];
@@ -71,15 +71,15 @@ export const examples: ExampleDef[] = [
 // Above will work fine for JIT, but not in AOT, so for now, define them
 // manually
 const routes: Routes = [
-  { path: "", component: IntroComponent, pathMatch: "full" },
-  { path: "ng_for", component: NgForExampleComponent, pathMatch: "full" },
-  { path: "ng_switch", component: NgSwitchExampleComponent, pathMatch: "full" },
-  { path: "ng_style", component: NgStyleExampleComponent, pathMatch: "full" },
-  { path: "ng_class", component: NgClassExampleComponent, pathMatch: "full" },
+  { path: '', component: IntroComponent, pathMatch: 'full' },
+  { path: 'ng_for', component: NgForExampleComponent, pathMatch: 'full' },
+  { path: 'ng_switch', component: NgSwitchExampleComponent, pathMatch: 'full' },
+  { path: 'ng_style', component: NgStyleExampleComponent, pathMatch: 'full' },
+  { path: 'ng_class', component: NgClassExampleComponent, pathMatch: 'full' },
   {
-    path: "ng_non_bindable",
+    path: 'ng_non_bindable',
     component: NgNonBindableExampleComponent,
-    pathMatch: "full"
+    pathMatch: 'full'
   }
 ];
 
@@ -97,9 +97,9 @@ const routes: Routes = [
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [
-    { provide: APP_BASE_HREF, useValue: "/" },
+    { provide: APP_BASE_HREF, useValue: '/' },
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: "ExampleDefs", useValue: examples }
+    { provide: 'ExampleDefs', useValue: examples }
   ],
   bootstrap: [AppComponent]
 })
